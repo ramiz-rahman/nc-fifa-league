366d0362538d8181144c10c64b1faa5c6eaaa008
ramiz-rahman <ramiz.rahman@outlook.com>
2018-11-16 11:36:48 +0600

    Create StandingsTable component
    The standings of each team must be displayed in a standings table. The
    stateful component is repsonsible for correctly rendering the current
    player rankings in sorted order.
    
    * Use dummy player data
    * Render player data from array of players

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import StandingsTable from './components/StandingsTable';
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <StandingsTable />
       </div>
     );
   }
---
+++ src/components/StandingsTable.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+class StandingsTable extends Component {
+  state = {
+    players: [
+      {
+        name: 'Ramiz',
+        club: 'Real Madrid',
+        mp: 12,
+        w: 10,
+        d: 2,
+        l: 0,
+        pts: 32,
+        gf: 36,
+        ga: 5,
+        gd: 31
+      },
+      {
+        name: 'Zulfikar',
+        club: 'Manchester United',
+        mp: 12,
+        w: 6,
+        d: 2,
+        l: 4,
+        pts: 20,
+        gf: 36,
+        ga: 5,
+        gd: 31
+      },
+      {
+        name: 'Sabit',
+        club: 'PSG',
+        mp: 12,
+        w: 6,
+        d: 2,
+        l: 4,
+        pts: 20,
+        gf: 36,
+        ga: 5,
+        gd: 31
+      }
+    ]
+  };
+
+  asTableCells(player) {
+    const playerData = Object.keys(player).map(key => (
+      <td key={key.toString()}>{player[key]}</td>
+    ));
+    return playerData;
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Club</th>
+            <th>MP</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>Pts</th>
+            <th>GF</th>
+            <th>GA</th>
+            <th>GD</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.players.map((player, i) => (
+            <tr key={i}>
+              <td key="rank">{i}</td>
+              {this.asTableCells(player)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default StandingsTable;
