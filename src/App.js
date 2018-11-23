5ac3a848b55ca280e12addc70e5a253bd8fb7499
ramiz-rahman <ramiz.rahman@outlook.com>
2018-11-23 16:16:43 +0600

    Add upcoming matches
    In the App component, create a method to return a list of upcoming
    matches. In the render method of the App component, return a list of
    upcoming matches.

--- src/App.js
+++ src/App.js
@@ -18,10 +18,49 @@ class App extends Component {
   state = {
     players: [],
     matches: [],
+    upcoming: [],
     showSideDrawer: false
   };
 
-  componentWillMount() {
+  _generateMatches = () => {
+    let upcoming = [];
+    let players = this.state.players;
+    players.sort((p1, p2) => {
+      if (p1.mp > p2.mp) return 1;
+      else if (p1.mp < p2.mp) return -1;
+      else return 0;
+    });
+
+    for (let i = 0; i < players.length; i++) {
+      for (let j = 1; j < players.length; j++) {
+        let homePlayerName = players[i].name;
+        let homePlayerClub = players[i].club;
+        let awayPlayerName = players[(i + j) % players.length].name;
+        let awayPlayerClub = players[(i + j) % players.length].club;
+        if (
+          this.state.matches.find(match => {
+            return (
+              match.homePlayerName === homePlayerName &&
+              match.awayPlayerName === awayPlayerName
+            );
+          })
+        ) {
+        } else {
+          upcoming.push({
+            homePlayerName,
+            homePlayerClub,
+            awayPlayerName,
+            awayPlayerClub
+          });
+          break;
+        }
+      }
+    }
+
+    return upcoming;
+  };
+
+  componentDidMount() {
     const playersQuery = fire.database().ref('players');
     playersQuery.on('value', snapshot => {
       let players = Object.values(snapshot.val());
@@ -40,6 +79,8 @@ class App extends Component {
         else return 0;
       });
       this.setState({ matches: matches });
+      const upcomingMatches = this._generateMatches();
+      this.setState({ upcoming: upcomingMatches });
     });
   }
 
@@ -98,6 +139,20 @@ class App extends Component {
 
           <AddMatchForm />
           <PlayerRegisterForm />
+
+          {this.state.upcoming.map((match, i) => (
+            <Match
+              key={`${match.homePlayerName}-${match.awayPlayerName}`}
+              homePlayerName={match.homePlayerName}
+              homePlayerClub={match.homePlayerClub}
+              awayPlayerName={match.awayPlayerName}
+              awayPlayerClub={match.awayPlayerClub}
+              homePlayerScore={null}
+              awayPlayerScore={null}
+              datetime={null}
+              number={this.state.matches.length + i + 1}
+            />
+          ))}
         </div>
       </div>
     );
